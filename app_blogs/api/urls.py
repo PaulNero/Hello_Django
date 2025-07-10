a281e2c8fb833c20c8e508506dc103fa4b8dfb96
PaulNero <58290662+PaulNero@users.noreply.github.com>
2025-07-10 21:41:24 +0300

    add api logic, crud methods for comments, tags, categories & post, add Dockerfile, docker-compose & deploy for gh actions

--- app_blogs/api/urls.py
+++ app_blogs/api/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter 
-from .views import CommentViewSet
+from .views import CommentViewSet, CategoryViewSet, TagsViewSet, PostViewSet
 
 app_name = "api/blogs"
 
 router = DefaultRouter()
 router.register('comments', CommentViewSet, basename='comments')
+router.register('categories', CategoryViewSet, basename='categories')
+router.register('tags', TagsViewSet, basename='tags')
+router.register('posts', PostViewSet, basename='posts')
 
 urlpatterns = router.urls
 
